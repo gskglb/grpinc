9a1c6bbf450294106a51ce4f8967c456a58df293
gururaj <guru4raj@gmail.com>
2016-06-06 21:55:09 +0530

    Proposal Delete implemented

--- client/templates/proposal/viewProposalDetails.js
+++ client/templates/proposal/viewProposalDetails.js
@@ -12,6 +12,25 @@ Template.viewProposalDetails.helpers({
   	proposalCreatedByUser : function(){
   		return Meteor.users.findOne({_id : this.createdBy});
   	},
+ 
+    onError: function () {
+      return function (error) { 
+      	Flash.danger("System ecountered error");
+      };
+    },
+    onSuccess: function () {
+      return function (result) { 
+      	Flash.success("Proposal Deleted successfully");
+      };
+    },
+    beforeRemove: function () {
+      return function () {
+        if (confirm('Really delete ?')) {
+          this.remove();
+        }
+      };
+    }
+
 });
 
 Template.viewProposalDetails.events({
--- client/templates/proposal/viewProposals.js
+++ client/templates/proposal/viewProposals.js
@@ -1,3 +1,23 @@
 Template.viewProposals.rendered = function() {
    $('[data-toggle="tooltip"]').tooltip();   
 };
+
+Template.viewProposals.helpers({
+    onError: function () {
+      return function (error) { 
+      	Flash.danger("System ecountered error");
+      };
+    },
+    onSuccess: function () {
+      return function (result) { 
+      	Flash.success("Proposal Deleted successfully");
+      };
+    },
+    beforeRemove: function () {
+      return function () {
+        if (confirm('Really delete ?')) {
+          this.remove();
+        }
+      };
+    }
+  });
